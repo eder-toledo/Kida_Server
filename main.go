f99bcef531f340f78f15a421b2a58226a1fbc197
Mauricio <andersson17_122013@outlook.com>
2018-03-04 00:12:14 -0600

    first commit

--- main.go
+++ main.go
@@ -5,17 +5,18 @@ import (
 	"net/http"
 )
 func home(w http.ResponseWriter, r *http.Request){
-	http.ServerFile(w,r,"index.html")
+	http.ServeFile(w,r,"index.html")
 }
 func main() {
 	fileServer := http.FileServer(http.Dir("public"))
 	http.Handle("/public/", http.StripPrefix("/public/", fileServer))
+	http.HandleFunc("/",home)
 
 port := os.Getenv("PORT")
 
 if port == "" {
 	port = "8000"
 }
-http.HandleFunc("/",home)
+
 http.ListenAndServe(":"+port,nil)
 }
