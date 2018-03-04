f1a802b7a8c04b72c898f1805ccd5f72b9304b8b
Mauricio <andersson17_122013@outlook.com>
2018-03-03 21:09:16 -0600

    first commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package ma
+
+import (
+	"os"
+	"net/http"
+)
+
+func main() {
+	fileServer := http.FileServer(http.Dir("public"))
+	http.Handle("/public/", http.StripPrefix("/public/", fileServer))
+}
+port := os.Getenv("PORT")
+
+if port == "" {
+	port = "8000"
+}
+
+http.ListenAndServe(":"+port,nil)
+
