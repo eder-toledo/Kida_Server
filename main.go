3c496e88555554289dfb7292d405a7bfb9f05fc8
Mauricio <andersson17_122013@outlook.com>
2018-03-06 22:28:35 -0600

    Rutas principales

--- main.go
+++ main.go
@@ -1,22 +1,53 @@
 package main
 
 import (
-	"os"
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 )
-func home(w http.ResponseWriter, r *http.Request){
-	http.ServeFile(w,r,"index.html")
+
+var db *sql.DB
+
+func api(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, " Ruta para /api ")
 }
 func main() {
-	fileServer := http.FileServer(http.Dir("public"))
-	http.Handle("/public/", http.StripPrefix("/public/", fileServer))
-	http.HandleFunc("/",home)
+	mux := http.NewServeMux()
+	fs := http.FileServer(http.Dir("public"))
+	mux.Handle("/", fs)
+	mux.HandleFunc("/api", api)
+	/*port := os.Getenv("PORT")
 
-port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}*/
+	server := &http.Server{
+		Addr:           ":8080",
+		Handler:        mux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	log.Println("Corriendo en el puerto 8080")
+	log.Fatal(server.ListenAndServe())
 
-if port == "" {
-	port = "8000"
-}
+	// Conectar a base de datos y comprobar error
 
-http.ListenAndServe(":"+port,nil)
+	db, err := sql.Open("mysql", "be7eddfee4e767: bdead22e @ /KIDADB") //cambiar por tus datos
+	if err != nil {
+		panic(err)
+	}
+	// db cerrar base de datos
+	defer db.Close()
+
+	// test para coneccion de base de datos
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
+
 }
